9bccb84e32c88e2f71c5becf847ac0e439a0e7b8
Phil Hennel <hennel@uni-bremen.de>
2025-05-02 18:00:54 +0200

    cleanup

--- custom_components/ble_scanner/parsers/base.py
+++
@@ -1,9 +0,0 @@
-from homeassistant.components.bluetooth import BluetoothServiceInfoBleak
-from typing import Optional, Dict, Any
-
-# --- Base Parser Class ---
-class BaseParser:
-    """Base class for BLE data parsers."""
-    def parse(self, service_info: BluetoothServiceInfoBleak) -> Optional[Dict[str, Any]]:
-        """Parse advertisement data. Must be implemented by subclasses."""
-        raise NotImplementedError
